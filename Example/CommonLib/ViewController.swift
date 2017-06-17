421d05a6654a488875af138bc6c18ae248e30553
gakaki <gakaki@gmail.com>
2017-06-17 16:15:55 +0800

    Initial commit

---
+++ Example/CommonLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CommonLib
+//
+//  Created by gakaki on 06/17/2017.
+//  Copyright (c) 2017 gakaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
